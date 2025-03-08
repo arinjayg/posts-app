6d7ca6fab2edb9f3870b083d8db934d19f6101fa
arinjayg <arinjay.gangwar100@gmail.com>
2025-03-08 19:25:07 +0530

    added posts page

---
+++ pages/posts/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="posts-page">
+    <h1>Posts</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.posts-page {
+  padding: 20px;
+
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
