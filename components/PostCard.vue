ba3b3ec69e884df5b773e5b3fe9cc5d0778a75e4
arinjayg <arinjay.gangwar100@gmail.com>
2025-03-08 19:56:11 +0530

    added PostCard component

---
+++ components/PostCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="post-card">
+    <div class="card-content">
+      <p class="pid">Post ID : {{ post.id }}</p>
+      <h2>Post Title : {{ post.title }}</h2>
+      <p>Post Details : {{ post.body.substring(0, 100) }}...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.post-card {
+  display: block;
+  padding: 15px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  transition: box-shadow 0.3s ease;
+
+  &:hover {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  }
+
+  .card-content {
+    .pid {
+      font-weight: 600;
+    }
+
+    h2 {
+      font-size: 1.2em;
+      margin-bottom: 10px;
+    }
+
+    p {
+      font-size: 0.9em;
+    }
+  }
+}
+</style>
