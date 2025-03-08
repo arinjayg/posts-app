6ed7c982471dcbfbaed6e561260a323094f215c3
arinjayg <arinjay.gangwar100@gmail.com>
2025-03-08 20:22:38 +0530

    fix: css and back btn added on detailed post page

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="post-detail">
-    {{ post }}
+    <p class="pid">Post ID : {{ post.id }}</p>
+    <h1>Post Title : {{ post.title }}</h1>
+    <p>Post Details : {{ post.body }}</p>
+    <nuxt-link to="/posts">
+      <button class="back-btn">
+        Back to Posts
+      </button>
+    </nuxt-link>
   </div>
 </template>
 
@@ -22,5 +29,36 @@ export default {
 <style scoped lang="scss">
 .post-detail {
   padding: 20px;
+
+  .pid {
+    font-weight: 600;
+  }
+
+  h1 {
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-bottom: 20px;
+  }
+
+  .back-btn {
+    width: fit-content;
+    height: auto;
+    padding: 15px;
+    border-radius: 20px;
+    background-color: #fff;
+    color: #043f06;
+    cursor: pointer;
+    transition: box-shadow 0.3s ease;
+
+    &:hover {
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+    }
+
+    @media (max-width: 768px) {
+      width: 100%;
+    }
+  }
 }
 </style>
